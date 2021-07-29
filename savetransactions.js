edddeb920f80099c646c746948541e388350367c
Master <masterservice2020@gmail.com>
2021-07-29 10:51:29 +0530

    Save Transactions Bug Fixed 1#_2nd Push

--- savetransactions.js
+++ savetransactions.js
@@ -9,33 +9,43 @@ db.loadDatabase(function (err) {    // Callback is optional
 
 var tr_data = require('./transaxns.json')
 
+
+
+/////get transactions
+
 var invoicenos = [
     "3120210727/9",
     "3120210724/26",
     "3120210726/11",
 
 ]
-// var i = 0
-// var strdt = '['
-// invoicenos.forEach(invoiceno => {
-//     db.findOne({ InvoiceNo: invoiceno}, function (err, docs) {
-//         console.log(docs)
-//         strdt += JSON.stringify(docs) + (i != 3 ? ',' : '')
-//         i++
-//         if (i == 3) {
-//             strdt += ']'
-//             fs.writeFile('./transaxns.json', strdt, err => {
-//                 if (err) {
-//                     console.error(err)
-//                     return
-//                 }
-//                 //file written successfully
-//             })
-//         }
-//     })
-// })
-// console.log(tr_data)
-axios.post('https://biz1pos.azurewebsites.net/api/Receipt/ordertransaction', tr_data).then(res => {
+var i = 0
+var strdt = '['
+var trnxns = []
+invoicenos.forEach(invoiceno => {
+    db.findOne({ InvoiceNo: invoiceno}, function (err, docs) {
+        console.log(docs)
+        trnxns.push(docs)
+        strdt += JSON.stringify(docs) + (i != 3 ? ',' : '')
+        i++
+        if (i == 3) {
+            strdt += ']'
+            fs.writeFile('./transaxns.json', strdt, err => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+                //file written successfully
+            })
+        }
+    })
+})
+console.log(trnxns)
+
+
+////Save Transactions
+
+axios.post('https://biz1pos.azurewebsites.net/api/Receipt/ordertransaction', trnxns).then(res => {
     console.log(`statusCode: ${res.data.status}`)
     console.log(res.data.error)
 }).catch(error => {
